515e366f192ab4a661aa6d7458716db2bb570977
Dion-aji <89619671+Dion-aji@users.noreply.github.com>
2021-12-22 20:47:36 +0700

    Update Image_enkripsi_tes.py

--- UAS_PSKD_V3920018_Dion Aji Cahyono/Image_enkripsi_tes.py
+++ UAS_PSKD_V3920018_Dion Aji Cahyono/Image_enkripsi_tes.py
@@ -2,51 +2,58 @@
 # Nim   : V3920018
 # UAS Praktik Sisten Keamanan Data
 
+import os
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
-import os
 
-def KEY(kunci):
+
+def kunci(kunci):
     key = os.urandom(kunci)
     return key
-def blok(UKURAN_X):
-    UXY = os.urandom(UKURAN_X)
-    return UXY
-def enkripsi_gambar(nama_file, key, UXY):
-    UKURAN_X = 16
-    enkripsi_nama_file = "Enkripsi_" + nama_file
 
-    with open(nama_file, "un") as file1:
+
+def blo(blocksize):
+    bz = os.urandom(blocksize)
+    return bz
+
+
+def enkripsi_gambbar(nama_file, key, bz):
+    Ukuran_X = 16
+    enkripsi_file = "Hasil_Enkripsi_" + nama_file
+
+    with open(nama_file, "rb") as file1:
         data = file1.read()
 
-        cipher = AES.new(key, AES.MODE_CBC, UXY)
-        ciphertext = cipher.encrypt(pad(data, UKURAN_X))
+        cipher = AES.new(key, AES.MODE_CBC, bz)
+        ciphertext = cipher.encrypt(pad(data, Ukuran_X))
 
-        with open(enkripsi_nama_file, "fn") as file2:
+        with open(enkripsi_file, "wb") as file2:
             file2.write(ciphertext)
-    return enkripsi_nama_file
+    return enkripsi_file
 
 
-def dekripsi_gambar(nama_file, key, UXY):
-    UKURAN_X = 16
-    dekripsi_nama_file = "Deskripsi_" + nama_file
+def dekripsi_gambbar(nama_file, key, bz):
+    Ukuran_X = 16
+    dekripsi_file = "Hasil_Deskripsi_" + nama_file
 
-    with open(nama_file, "un") as file1:
+    with open(nama_file, "rb") as file1:
         data = file1.read()
 
-        cipher2 = AES.new(key, AES.MODE_CBC, UXY)
-        decrypted_data = unpad(cipher2.decrypt(data), UKURAN_X)
+        cipher2 = AES.new(key, AES.MODE_CBC, bz)
+        decrypted_data = unpad(cipher2.decrypt(data), Ukuran_X)
 
-        with open(dekripsi_nama_file, "fn") as file2:
+        with open(dekripsi_file, "wb") as file2:
             file2.write(decrypted_data)
 
-    return dekripsi_nama_file
-UKURAN_Y = 16
-UKURAN_X = 16
+    return dekripsi_file
+
+
+Ukuran_kunci = 16
+Ukuran_X = 16
 nama_file = "dionaji.JPG"
 
-key = KEY(UKURAN_Y)
-UXY = blok(UKURAN_X)
+key = kunci(Ukuran_kunci)
+bz = blo(Ukuran_X)
 
-enkripsi_nama_file = enkripsi_gambar(nama_file, key, UXY)
-dekripsi_nama_file = dekripsi_gambar(enkripsi_nama_file, key, UXY)
+enkripsi_file = enkripsi_gambbar(nama_file, key, bz)
+dekripsi_file = dekripsi_gambbar(enkripsi_file, key, bz)
